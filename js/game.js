d986921bd1aa6cd0e4a4aa91d9f2911bbb1f37c9
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-05-29 01:39:41 +0400

    Finished game logic

--- js/game.js
+++ js/game.js
@@ -1,22 +1,55 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
+// Buttons
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+
+// images
+const playerVisual = document.querySelector("#signPlayer");
+const compVisual = document.querySelector("#signComp");
+
+//
+const player = document.querySelector("#playerscore");
+const computer = document.querySelector("#computerscore");
+
+// targeting backdrop and nedResult
+const backdrop = document.querySelector("#backdrop");
+const modalCard = document.querySelector("#addModal");
+const endResult = document.querySelector(".endResult");
+const restartBtn = document.querySelector(".restartBtn");
+
+// starting point for scores
+let playerScore = 0;
+let computerScore = 0;
 
 const choices = ["Rock", "Paper", "Scissors"];
 
-const computerChoice = getRandomChoice(choices);
 let playerChoice = "";
 
 rock.addEventListener("click", function () {
+  const computerChoice = getRandomChoice(choices);
   playRound("Rock", computerChoice);
 });
 
 paper.addEventListener("click", function () {
+  const computerChoice = getRandomChoice(choices);
   playRound("Paper", computerChoice);
 });
 
 scissor.addEventListener("click", function () {
-  playRound("Scissor", computerChoice);
+  const computerChoice = getRandomChoice(choices);
+  playRound("Scissors", computerChoice);
+});
+
+restartBtn.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  player.innerText = 0;
+  computer.innerText = 0;
+  toggleModal();
+});
+
+backdrop.addEventListener("click", () => {
+  toggleModal();
 });
 
 function getRandomChoice(choices) {
@@ -24,29 +57,61 @@ function getRandomChoice(choices) {
   return choices[randomIndex];
 }
 
-function play(choice) {
-  playRound("Rock", computerChoice);
+function toggleModal() {
+  backdrop.classList.toggle("visible");
+  modalCard.classList.toggle("visible");
 }
 
 function playRound(playerChoice, computerChoice) {
-  console.log("Computers choice: " + computerChoice);
-  console.log("Players choice: " + playerChoice);
-
-  if (playerChoice === "Rock" && computerChoice === "Scissors") {
-    console.log("You win, Rock beats Scissors");
+  if (playerScore >= 5 || computerScore >= 5) {
+    toggleModal();
+    return;
+  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+    playerScore++;
+    player.innerText = playerScore;
+    playerVisual.innerText = String.fromCodePoint(0x270a);
+    compVisual.innerText = String.fromCodePoint(0x270c);
   } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-    console.log("You win, Paper beats Rock");
+    playerScore++;
+    player.innerText = playerScore;
+    playerVisual.innerText = String.fromCodePoint(0x1f91a);
+    compVisual.innerText = String.fromCodePoint(0x270a);
   } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-    console.log("You win, Scissors beat Paper");
+    playerScore++;
+    player.innerText = playerScore;
+    playerVisual.innerText = String.fromCodePoint(0x270c);
+    compVisual.innerText = String.fromCodePoint(0x1f91a);
   } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-    console.log("You lose, Paper beats Rock");
+    computerScore++;
+    computer.innerText = computerScore;
+    playerVisual.innerText = String.fromCodePoint(0x270a);
+    compVisual.innerText = String.fromCodePoint(0x1f91a);
   } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
-    console.log("You lose, Scissors beat Paper");
+    computerScore++;
+    computer.innerText = computerScore;
+    playerVisual.innerText = String.fromCodePoint(0x1f91a);
+    compVisual.innerText = String.fromCodePoint(0x270c);
   } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
-    console.log("You lose, Rock beats Scissors");
-  } else if (playerChoice === computerChoice) {
-    console.log("It's a tie!");
-  } else {
-    console.log("Invalid input. Please enter Rock, Paper, or Scissors.");
+    computerScore++;
+    computer.innerText = computerScore;
+    playerVisual.innerText = String.fromCodePoint(0x270c);
+    compVisual.innerText = String.fromCodePoint(0x270a);
+  } else if (playerChoice === "Rock" && computerChoice === "Rock") {
+    playerVisual.innerText = String.fromCodePoint(0x270a);
+    compVisual.innerText = String.fromCodePoint(0x270a);
+  } else if (playerChoice === "Paper" && computerChoice === "Paper") {
+    playerVisual.innerText = String.fromCodePoint(0x1f91a);
+    compVisual.innerText = String.fromCodePoint(0x1f91a);
+  } else if (playerChoice === "Scissors" && computerChoice === "Scissors") {
+    playerVisual.innerText = String.fromCodePoint(0x270c);
+    compVisual.innerText = String.fromCodePoint(0x270c);
+  }
+
+  if (playerScore == 5 || playerScore > 5) {
+    endResult.innerText = "You won!";
+    toggleModal();
+  } else if (computerScore == 5) {
+    endResult.innerText = "You lost...";
+    toggleModal();
   }
 }
